0645493628c3a9b4dff111dc441c2976ebacbb4f
Aleksandar Kurtakov <akurtakov@gmail.com>
2025-04-15 11:17:46 +0300

    Use builtin method to call npm
    Fixes calling LS install when no system nodejs is found. Bonus points
    for really simplifying the code.
    Enabled save actions to automate organize imports.

--- org.eclipse.shellwax.core/src/org/eclipse/shellwax/internal/BashLanguageServer.java
+++ org.eclipse.shellwax.core/src/org/eclipse/shellwax/internal/BashLanguageServer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2024 Red Hat Inc. and others.
+ * Copyright (c) 2019, 2025 Red Hat Inc. and others.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -14,16 +14,12 @@ package org.eclipse.shellwax.internal;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.wildwebdeveloper.embedder.node.NodeJSManager;
 
 public class BashLanguageServer extends ProcessStreamConnectionProvider {
@@ -31,7 +27,6 @@ public class BashLanguageServer extends ProcessStreamConnectionProvider {
 	private static final String LOCAL_PATH = "/.local/share/shellwax/"+LS_VERSION;
 	private static final String LS_MAIN = "/node_modules/.bin/bash-language-server";
 
-	private static boolean alreadyWarned;
 	private static CompletableFuture<Void> initializeFuture;
 
 	private static String getLsPath() {
@@ -49,9 +44,7 @@ public class BashLanguageServer extends ProcessStreamConnectionProvider {
 	public BashLanguageServer() {
 		List<String> commands = new ArrayList<>();
 		String nodePath = NodeJSManager.getNodeJsLocation().getAbsolutePath();
-		if (nodePath == null) {
-			nodePath = getExecLocation("node");
-		}
+		
 		if (nodePath != null) {
 			if (!isInstalled()) {
 				installLS();
@@ -98,61 +91,17 @@ public class BashLanguageServer extends ProcessStreamConnectionProvider {
 					File nodeModulesDir = new File(installLocation, "node_modules");
 					nodeModulesDir.mkdir();
 				}
-				String npmPath = NodeJSManager.getNpmLocation().getAbsolutePath();
-				if (npmPath == null) {
-					npmPath = getExecLocation("npm");
-				}
-				if (Platform.OS.isWindows() && !npmPath.endsWith(".cmd")) {
-					npmPath = npmPath+".cmd";
-				}
-				if (npmPath != null) {
-					List<String> commands = List.of(npmPath, "install","--prefix=.","bash-language-server@"+LS_VERSION);
-					ProcessBuilder pb = new ProcessBuilder(commands);
-					pb.directory(installLocation);
-					pb.inheritIO(); // Redirects stdout and stderr to System.out
-					try {
-						Process ps = pb.start();
-						ps.waitFor();
-					} catch (IOException | InterruptedException e) {
-						e.printStackTrace();
-					}
+				ProcessBuilder pb = NodeJSManager.prepareNPMProcessBuilder("install","--prefix=.","bash-language-server@"+LS_VERSION);
+				pb.directory(installLocation);
+				pb.inheritIO(); // Redirects stdout and stderr to System.out
+				try {
+					Process ps = pb.start();
+					ps.waitFor();
+				} catch (IOException | InterruptedException e) {
+					e.printStackTrace();
 				}
 			});
 		}
 		return initializeFuture;
-	}
-
-	private static String getExecLocation(String exec) {
-		String res = "/path/to/" + exec;
-		String[] command = { "/bin/bash", "-c", "which " + exec };
-        if (Platform.OS.isWindows()) {
-			command = new String[] { "cmd", "/c", "where " + exec };
-		}
-		try {
-			Process p = Runtime.getRuntime().exec(command);
-			res = p.inputReader().readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		// Try default install path as last resort
-        if (res == null && Platform.OS.isMac()) {
-			res = "/usr/local/bin/" + exec;
-		}
-
-		if (res != null && Files.exists(Paths.get(res))) {
-			return res;
-		} else if (!alreadyWarned) {
-			warnExecMissing(exec);
-			alreadyWarned = true;
-		}
-		return null;
-	}
-
-	private static void warnExecMissing(String exec) {
-		Display.getDefault().asyncExec(() ->
-			MessageDialog.openWarning(Display.getCurrent().getActiveShell(), "Missing " + exec,
-					"Could not find node.js. This will result in editors missing key features.\n"
-							+ "Please make sure node.js is installed and that your PATH environement variable contains the location to the `node` executable."));
 	}
 }
